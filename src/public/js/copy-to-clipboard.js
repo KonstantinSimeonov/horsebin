6f1448f1d586e75456825caf1f0b8f94d123935f
Konstantin Simeonov <kon.simeonov@gmail.com>
2016-11-06 06:55:55 +0200

    Added paste fetch as file client side

---
+++ src/public/js/copy-to-clipboard.js
@@ -0,0 +1,13 @@
+$(function () {
+    const $tmpInput = $('<textarea />'),
+        $cloneBtn = $('#clone-btn'),
+        $body = $(document.body);
+
+    $cloneBtn.on('click', function () {
+        $body.append($tmpInput);
+        $tmpInput.val($('code').text());
+        $tmpInput.select();
+        document.execCommand('copy');
+        $tmpInput.remove();
+    });
+});
---
+++ src/public/js/fetch-as-file.js
@@ -0,0 +1,31 @@
+$(function () {
+    // TODO: expand
+    const filetypeMap = {
+        'javascript': 'js',
+        'haskell': 'hs'
+    };
+
+
+    const $fetchBtn = $('#fetch-btn'),
+        $container = $('#paste-details');
+
+    $fetchBtn.on('click', function () {
+        const contents = $container.find('code').text(),
+            pasteName = $container.find('.paste-name').text();
+
+        let type = $container.find('pre').attr('class').split('-').pop();
+
+        if(filetypeMap[type]) {
+            type = filetypeMap[type];
+        }
+
+        window.URL = window.URL || window.webkitURL;
+        const blob = new Blob([contents], {type: 'octet/stream'});
+
+        blob.lastModifiedDate = new Date();
+        blob.name = pasteName;
+
+        $fetchBtn.attr('download', pasteName + '.' + type);
+        $fetchBtn.attr('href', window.URL.createObjectURL(blob));
+    });
+});
