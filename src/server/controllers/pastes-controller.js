ae72f8e64cc6e9365e64887d6612812f463c3f80
Konstantin Simeonov <kon.simeonov@gmail.com>
2016-11-09 20:00:18 +0200

    Added listing for pastes

--- src/server/controllers/pastes-controller.js
+++ src/server/controllers/pastes-controller.js
@@ -3,6 +3,18 @@
 const pastesServices = require('../data/pastes-services'),
     langServices = require('../data/languages');
 
+function getNthIndex(n, symbol, str) {
+    let index = 0,
+        count = 0;
+
+    while(count < n && index < str.length) {
+        index = str.indexOf(symbol, index + 1);
+        count += 1;
+    }
+    console.log(index);
+    return index;
+}
+
 module.exports = {
     byId(req, res) {
         const paste = req.paste;
@@ -58,7 +70,11 @@ module.exports = {
         pastesServices
             .pastesByUser(user_id)
             .then(pastes => {
-                res.status(200).json(pastes);
+                const shortenedPastes = pastes.map(p => {
+                    p.content = p.content.slice(0, getNthIndex(5, '\n', p.content));
+                    return p;
+                });
+                res.status(200).render('list-pastes', { user: req.user, pastes });
             })
             .catch(error => {
                 console.log(error);
