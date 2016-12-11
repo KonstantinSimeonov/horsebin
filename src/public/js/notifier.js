e97952c0127fa69f588dad8f73c2549706af00ce
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-12-11 16:47:35 +0200

    Added notifier for message flashing

---
+++ src/public/js/notifier.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const notifier = (() => {
+
+    const DEFAULT_FADEOUT = 3000;
+
+    const $success = $(`<div class="alert alert-dismissible alert-success fading">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Success!</strong> <em class="alert-msg"></em>
+    </div>`),
+        $failure = $(`<div class="alert alert-dismissible alert-danger fading">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Error!</strong>
+        <em class="alert-msg"></em>`),
+        $alertsContainer = $('<div id="alerts-container" />').appendTo($('body'));
+
+    function showAlert($alertElement, message, fadeTime) {
+        $alertElement.find('.alert-msg').text(message);
+        $alertElement.appendTo($alertsContainer);
+
+        setTimeout(() => {
+            $alertElement.addClass('out');
+        }, +fadeTime || DEFAULT_FADEOUT);
+    }
+
+    return {
+        success: showAlert.bind(null, $success),
+        failure: showAlert.bind(null, $failure)
+    }
+})();
