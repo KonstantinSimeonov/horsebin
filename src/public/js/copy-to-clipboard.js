2a8b86d103dd83a536bf0f0bb592766c21eafbb9
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-06 20:52:28 +0200

    Made clipboard copying reusable as jquery plugin

--- src/public/js/copy-to-clipboard.js
+++ src/public/js/copy-to-clipboard.js
@@ -1,15 +1,21 @@
 'use strict';
 
-$(function () {
-    const $tmpInput = $('<textarea />'),
-        $cloneBtn = $('#clone-btn'),
-        $body = $(document.body);
+$(() => {
+    $.fn.copyToClipboardBtn = function (textContainerSelector) {
+        const $this = $(this),
+            $container = $(textContainerSelector);
 
-    $cloneBtn.on('click', function () {
-        $body.append($tmpInput);
-        $tmpInput.val($('code').text());
-        $tmpInput.select();
-        document.execCommand('copy');
-        $tmpInput.remove();
-    });
+        const $tmpInput = $('<textarea />'),
+            $body = $(document.body);
+
+        $this.on('click', function () {
+            $body.append($tmpInput);
+            $tmpInput.val($(textContainerSelector).text());
+            $tmpInput.select();
+            document.execCommand('copy');
+            $tmpInput.remove();
+        });
+    }
+
+    $('#clone-btn').copyToClipboardBtn('code');
 });
