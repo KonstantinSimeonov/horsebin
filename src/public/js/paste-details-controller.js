42fb4c203ceb57a6db2b46d972b1eaa6450e1065
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-13 21:45:51 +0200

    Fixed bug with embedding

--- src/public/js/paste-details-controller.js
+++ src/public/js/paste-details-controller.js
@@ -2,11 +2,10 @@
 
 $(() => {
     $('#clone-btn').copyToClipboardBtn('code');
-    console.log('hey');
     $('#copy-url-btn').copyToClipboardBtn(() => window.location.href);
     
     const $embedBtn = $('#btn-get-embedded');
-    const rawUrl = window.location.host + $embedBtn.data('url');
+    const rawUrl = 'https://' + window.location.host + $embedBtn.data('url');
     $embedBtn.copyToClipboardBtn(() => `<iframe width="560" height="315" src="${rawUrl}" frameborder="0" allowfullscreen></iframe>`);
 
     $('#edit-btn').on('click', () => {
