83b7dc90acc43d38767159c5fca4e7f414d6fe2a
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-13 23:45:55 +0200

    File extensions map

--- src/public/js/fetch-as-file.js
+++ src/public/js/fetch-as-file.js
@@ -6,33 +6,78 @@ $(function () {
         'javascript': 'js',
         'haskell': 'hs',
         'csharp': 'cs',
-        'c++': 'cpp',
-        'c': 'c',
-        'java': 'java',
+        'markup': 'md',
+        'javascript': 'js',
+        'actionscript': 'as',
+        'ada': 'adb',
+        'aspnet': 'aspx',
+        'bash': 'sh',
+        'batch': 'bat',
+        'csharp': 'cs',
+        'coffeescript': 'coffee',
+        'crystal': 'cr',
+        'fsharp': 'fs',
+        'jade': 'pug',
+        'eiffel': 'e',
+        'elixir': 'exs',
+        'erlang': 'erl',
+        'fortran': 'f95',
+        'haxe': 'hx',
+        'icon': 'icn',
+        'jolie': 'ol',
+        'julia': 'jl',
+        'kotlin': 'kt',
+        'latex': 'tex',
+        'livescript': 'ls',
+        'lolcode': 'lol',
+        'markdown': 'md',
+        'matlab': 'm',
+        'nsis': 'nsh',
+        'objectivec': 'm',
+        'ocaml': 'ml',
+        'pascal': 'pas',
+        'perl': 'pl',
+        'powershell': 'ps1',
+        'processing': 'pde',
+        'prolog': 'pro',
+        'protobuf': 'proto',
+        'puppet': 'pp',
+        'python': 'py',
+        'qore': 'qm',
+        'ruby': 'rb',
+        'rust': 'rs',
+        'sass': 'scss',
+        'scheme': 'ss',
+        'smalltalk': 'st',
+        'stylus': 'styl',
+        'typescript': 'ts',
+        'verilog': 'v',
+        'vhdl': 'vhd',
+        'xojo': 'xojo_code'
     };
 
-    const $fetchBtn = $('#fetch-btn'),
-        $container = $('#paste-details');
+const $fetchBtn = $('#fetch-btn'),
+    $container = $('#paste-details');
 
-    $fetchBtn.on('click', function () {
-        const contents = $container.find('code').text(),
-            pasteName = $container.find('.paste-name').text(),
-            language = $container
-                            .find('pre')
-                            .attr('class')
-                            .split('-')
-                            .pop(),
-            fileExtension = filetypeMap[language] || language;
+$fetchBtn.on('click', function () {
+    const contents = $container.find('code').text(),
+        pasteName = $container.find('.paste-name').text(),
+        language = $container
+            .find('pre')
+            .attr('class')
+            .split('-')
+            .pop(),
+        fileExtension = filetypeMap[language] || language;
 
-        
 
-        window.URL = window.URL || window.webkitURL;
-        const blob = new Blob([contents], {type: 'octet/stream'});
 
-        blob.lastModifiedDate = new Date();
-        blob.name = pasteName;
+    window.URL = window.URL || window.webkitURL;
+    const blob = new Blob([contents], { type: 'octet/stream' });
 
-        $fetchBtn.attr('download', pasteName + '.' + fileExtension);
-        $fetchBtn.attr('href', window.URL.createObjectURL(blob));
-    });
+    blob.lastModifiedDate = new Date();
+    blob.name = pasteName;
+
+    $fetchBtn.attr('download', pasteName + '.' + fileExtension);
+    $fetchBtn.attr('href', window.URL.createObjectURL(blob));
+});
 });
