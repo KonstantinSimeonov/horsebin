68ee7f01cd5a59d8d29cc872775b4971f9a1db31
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-13 22:00:30 +0200

    Cosmetic changes

--- src/public/js/paste-details-controller.js
+++ src/public/js/paste-details-controller.js
@@ -6,7 +6,9 @@ $(() => {
     
     const $embedBtn = $('#btn-get-embedded');
     const rawUrl = 'https://' + window.location.host + $embedBtn.data('url');
-    $embedBtn.copyToClipboardBtn(() => `<iframe width="560" height="315" src="${rawUrl}" frameborder="0" allowfullscreen></iframe>`);
+    const height = $('pre code').css('height');
+    console.log(height);
+    $embedBtn.copyToClipboardBtn(() => `<iframe width="600" height="${height}" style="font-size: 18px;" src="${rawUrl}" frameborder="0" allowfullscreen></iframe>`);
 
     $('#edit-btn').on('click', () => {
         const $editForm = $('.form-edit-paste');
