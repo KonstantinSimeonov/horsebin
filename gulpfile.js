d17afeab32f9f395389b366c496089be57be3c01
Konstantin Simeonov <kon.simeonov@gmail.com>
2017-01-15 13:49:44 +0200

    Added build for paste themes from prism

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,12 @@ const gulp = require('gulp'),
     minifyCss = require('gulp-clean-css'),
     babel = require('gulp-babel');
 
-gulp.task('default', () => gulp
+gulp.task('paste-themes', () => gulp
+                                    .src('./src/public/bower_components/prism/themes/*.css')
+                                    .pipe(minifyCss())
+                                    .pipe(gulp.dest('../npaste-build/public/bower_components/prism/themes/')));
+
+gulp.task('bundles', () => gulp
                             .src('./src/views/*.dust')
                             .pipe(useref({ searchPath: './src' }))
                             .pipe(gulpif('*.js', babel({ presets: ['es2015'] })))
@@ -16,3 +21,5 @@ gulp.task('default', () => gulp
                             .pipe(gulpif('*.css', minifyCss()))
                             .pipe(gulpif('*.css', gulp.dest('../npaste-build/')))
                             .pipe(gulpif('*.dust', gulp.dest('../npaste-build/views'))));
+
+gulp.task('default', ['paste-themes', 'bundles']);
--- src/public/js/paste-details-controller.js
+++ src/public/js/paste-details-controller.js
@@ -3,11 +3,11 @@
 $(() => {
     $('#clone-btn').copyToClipboardBtn('code');
     $('#copy-url-btn').copyToClipboardBtn(() => window.location.href);
-    
-    const $embedBtn = $('#btn-get-embedded');
-    const rawUrl = 'https://' + window.location.host + $embedBtn.data('url');
-    const height = $('pre code').css('height');
-    console.log(height);
+
+    const $embedBtn = $('#btn-get-embedded'),
+        rawUrl = 'https://' + window.location.host + $embedBtn.data('url'),
+        height = $('pre code').css('height');
+
     $embedBtn.copyToClipboardBtn(() => `<iframe width="600" height="${height}" style="font-size: 18px;" src="${rawUrl}" frameborder="0" allowfullscreen></iframe>`);
 
     $('#edit-btn').on('click', () => {
